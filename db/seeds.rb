6482f9ab693b3790c7de99ec0c45aca098070534
Mustafa Tahir <mustafatahir@hotmail.co.uk>
2022-10-22 15:44:13 +0100

    api seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require "json"
+require "open-uri"
+
+puts "Cleaning database..."
+Movie.destroy_all
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+movie_serialised = URI.open(url).read
+movies = JSON.parse(movie_serialised)
+
+movie_results = movies['results']
+
+puts "creating movies"
+
+movie_results.each do |movie|
+  title = movie['title']
+  overview = movie['overview']
+  rating = movie['vote_average']
+  poster_url = movie['poster_path']
+  movie = Movie.new(title: title, overview: overview, rating: rating, poster_url: poster_url)
+  movie.save
+end
+
+puts "movies done"
