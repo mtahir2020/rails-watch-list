fd0d9f809d70884f7365e4c7ed874986ddea004f
Mustafa Tahir <mustafatahir@hotmail.co.uk>
2022-11-06 18:58:39 +0000

    grid changes

--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -1,9 +1,16 @@
 module ListsHelper
+  # def string_half(string)
+  #   overview_string = string.split
+  #   new_string = overview_string.each_slice((overview_string.size / 2.0).round).to_a
+  #   first_half = new_string[0].join(' ')
+  #   second_half = new_string[1].join(' ')
+  #   strings = [first_half, second_half]
+  # end
+
   def string_half(string)
     overview_string = string.split
-    new_string = overview_string.each_slice((overview_string.size / 2.0).round).to_a
-    first_half = new_string[0].join(' ')
-    second_half = new_string[1].join(' ')
+    first_half = overview_string.take(15).join(' ')
+    second_half = overview_string.drop(15).join(' ')
     strings = [first_half, second_half]
   end
 end
